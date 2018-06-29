3a9c5cb8e4b539525470228af9152d549adf2284
Aaron Tucker <aarondtucker@gmail.com>
2018-06-29 14:31:59 -0700

    break out the policy code into its own file

---
+++ policies.py
@@ -0,0 +1,110 @@
+import numpy as np
+from baselines.ppo2.ppo2 import Model
+from . import environments
+import os
+import os.path as osp
+import joblib
+
+class Policy:
+    """
+    Lets us save, restore, and step a policy forward
+
+    Plausibly we'll want to start passing a SerializationContext argument
+    instead of save_dir, so that we can abstract out the handling of the
+    load/save logic, and ensuring that the relevant directories exist.
+
+    If we do that we'll have to intercept the Model's use of joblib,
+    maybe using a temporary file.
+    """
+    model_args_fname = 'model_args.pkl'
+    model_fname = 'model' # extension assigned automatically
+    annotations_fname = 'annotations.pkl'
+
+    def __init__(self, model_args):
+        self.model_args = model_args
+        self.model = Model(**model_args)
+        self.annotations = {}
+
+    def step(self, obs):
+        return self.model.step(obs)
+
+    def save(self, save_dir, **kwargs):
+        """
+        Saves a policy, along with other annotations about it
+
+        Args:
+            save_dir: directory to put the policy in
+            **kwargs: annotations that you want to store along with the model
+                kind of janky interface, but seems maybe not-that-bad
+                canonical thing that we'll want to store is the training reward
+        """
+        self.annotations.update(kwargs)
+        os.makedirs(save_dir, exist_ok=True)
+        joblib.dump(self.model_args, osp.join(save_dir, self.model_args_fname))
+        self.model.save(osp.join(osp.join(save_dir, self.model_fname)))
+        joblib.dump(self.annotations, osp.join(save_dir, self.annotations_fname))
+
+    @classmethod
+    def load(cls, save_dir):
+        """
+        Restore a model completely from storage.
+
+        This needs to be a class method, so that we don't have to initialize
+        a policy in order to get the parameters back.
+        """
+        model_args = joblib.load(osp.join(save_dir, cls.model_args_fname))
+        policy = cls(model_args)
+        policy.model.load(osp.join(save_dir, cls.model_fname))
+        policy.annotations = joblib.load(osp.join(save_dir, cls.annotations_fname))
+        return policy
+
+
+class EnvPolicy(Policy):
+    """
+    Lets us save and restore a policy where the policy depends on some sort of
+    modification to the environment, expressed as an environment wrapper.
+
+    Unfortunately, we still need to initialize the same type of environment
+    before we can open it here, so that it has a chance
+    """
+    env_params_fname = 'env_params.pkl'
+
+    def __init__(self, model_args, envs=None):
+        super().__init__(model_args)
+        self.envs = envs
+
+    def save(self, save_dir, **kwargs):
+        assert self.envs
+        super().save(save_dir, **kwargs)
+        env_params = environments.serialize_env_wrapper(self.envs)
+        joblib.dump(env_params, osp.join(save_dir, self.env_params_fname))
+
+    @classmethod
+    def load(cls, save_dir, envs):
+        import pickle
+        policy = super().load(save_dir)
+        # we save the pickle-serialized env_params, so we need pickle to deserialize them
+        env_params = pickle.loads(joblib.load(osp.join(save_dir, cls.env_params_fname)))
+        policy.envs = environments.restore_serialized_env_wrapper(env_params, envs)
+        environments.make_const(policy.envs)
+        return policy
+
+
+def run_policy(*, model, environments, render=True):
+    # Initialize the stuff we want to keep track of
+    rewards = []
+
+    # Initialize our environment
+    done = [False]
+    obs = np.zeros((environments.num_envs,) + environments.observation_space.shape)
+    obs[:] = environments.reset()
+
+    # run the policy until done
+    while not any(done):
+        actions, _, _, _ = model.step(obs)
+        obs[:], reward, done, info = environments.step(actions)
+        rewards.append(reward)
+        if render:
+            environments.render()
+
+    return sum(rewards[:])
