6918abe79ef20df3548b3d416b7eb8d85e7789fb
Aaron Tucker <aarondtucker@gmail.com>
2018-06-29 16:49:34 -0700

    copy over a wrapper that lets us run gym environments as rllab environments

--- environments.py
+++ environments.py
@@ -1,5 +1,7 @@
 import pickle
 import numpy as np
+from rllab.envs.base import Env
+from rllab.envs.gym_env import convert_gym_space
 
 class ConstantStatistics(object):
     def __init__(self, running_mean):
@@ -36,3 +38,35 @@ def make_const(norm):
     for k, v in norm.__dict__.items():
         if hasattr(v, 'update_from_moments'):
             setattr(norm, k, ConstantStatistics(v))
+
+
+# Copied from https://github.com/HumanCompatibleAI/population-irl/blob/master/pirl/irl/airl.py
+# this hacks around airl being built on top of rllib, and not using gym
+# environments
+class VecGymEnv(Env):
+    def __init__(self, venv):
+        self.venv = venv
+        self._observation_space = convert_gym_space(venv.observation_space)
+        self._action_space = convert_gym_space(venv.action_space)
+
+    @property
+    def observation_space(self):
+        return self._observation_space
+
+    @property
+    def action_space(self):
+        return self._action_space
+
+    def terminate(self):
+        # Normally we'd close environments, but pirl.experiments handles this.
+        pass
+
+    @property
+    def vectorized(self):
+        return True
+
+    def vec_env_executor(self, n_envs, max_path_length):
+        # SOMEDAY: make these parameters have an effect?
+        # We're powerless as the environments have already been created.
+        # But I'm not too bothered by this, as we can tweak them elsewhere.
+        return self.venv
