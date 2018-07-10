5f023295155ec0c18319c0eadb95a1323a1d4c81
Aaron Tucker <aarondtucker@gmail.com>
2018-07-10 16:55:28 -0700

    break things out into a context so that we can do more prototyping from ipython

--- atari_irl/behavioral_cloning.py
+++ atari_irl/behavioral_cloning.py
@@ -13,32 +13,57 @@ def relabel_trajectory_observations(policy, trajectory):
     return trajectory
 
 
-def dagger(
-        policy, envs, policy_class,
-        *,
-        total_timesteps, nsteps=2048, noptepochs=4, nminibatches=32
-):
-    batching_config = training.make_batching_config(
-        env=envs,
-        nsteps=nsteps, noptepochs=noptepochs, nminibatches=nminibatches
-    )
-    policy_clone = policy_class(
-        sess=tf.get_default_session(),
-        ob_space=envs.observation_space,
-        ac_space=envs.action_space,
-        nbatch=batching_config.nbatch,
-        nsteps=batching_config.nsteps
-    )
-    trajectories = policies.sample_trajectories(
-        model=policy.model, environments=envs, n_trajectories=10
-    )
+class CloningContext:
+    def __init__(
+            self, policy, envs, policy_class, *,
+            nsteps=2048, noptepochs=4, nminibatches=32, n_trajectories=10
+    ):
+        self.base_policy = policy
+        self.envs = envs
 
-    for t in range(total_timesteps // batching_config.nbatch):
-        update_policy(policy_clone, trajectories)
-        cloned_policy_trajectories_t = policies.sample_trajectories(
-            model=policy_clone, environments=envs, n_trajectories=10
+        self.n_trajectorie =n_trajectories
+        self.batching_config = training.make_batching_config(
+            env=envs,
+            nsteps=nsteps, noptepochs=noptepochs, nminibatches=nminibatches
         )
-        trajectories += relabel_trajectory_observations(
-            cloned_policy_trajectories_t
+
+        self.policy_clone = policy_class(
+            sess=tf.get_default_session(),
+            ob_space=envs.observation_space,
+            ac_space=envs.action_space,
+            nbatch=self.batching_config.nbatch,
+            nsteps=self.batching_config.nsteps
         )
+
+    def __enter__(self):
+        self.base_trajectories = policies.sample_trajectories(
+            model=self.base_policy, environments=self.envs,
+            n_trajectories=self.n_trajectories
+        )
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+
+def dagger(
+        policy, envs, policy_class, *, total_timesteps,
+        nsteps=2048, noptepochs=4, nminibatches=32, n_trajectories=10,
+):
+    with CloningContext(
+            policy, envs, policy_class,
+            nsteps=nsteps, noptepochs=noptepochs, nminibatches=nminibatches,
+            n_trajectories=n_trajectories
+    ) as context:
+        trajectories = context.base_trajectories.copy()
+        for t in range(total_timesteps // context.batching_config.nbatch):
+            update_policy(context.policy_clone, trajectories)
+            cloned_policy_trajectories_t = policies.sample_trajectories(
+                model=context.policy_clone,
+                environments=envs,
+                n_trajectories=n_trajectories
+            )
+            trajectories += relabel_trajectory_observations(
+                cloned_policy_trajectories_t
+            )
 
