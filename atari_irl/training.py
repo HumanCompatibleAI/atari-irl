43dc446b338e0f468f692fb1c0f15d2d8abb8bee
Aaron Tucker <aarondtucker@gmail.com>
2018-08-14 15:31:38 -0700

    get rid of an unneeded print statement

--- atari_irl/training.py
+++ atari_irl/training.py
@@ -72,7 +72,6 @@ def print_log(
     logger.logkv("explained_variance", float(ev))
     logger.logkv('eprewmean', safemean([epinfo['r'] for epinfo in epinfobuf]))
     logger.logkv('eplenmean', safemean([epinfo['l'] for epinfo in epinfobuf]))
-    print(safemean([epinfo['r'] for epinfo in epinfobuf]))
     logger.logkv('time_elapsed', tnow - tfirststart)
     for (lossval, lossname) in zip(lossvals, model.loss_names):
         logger.logkv(lossname, lossval)
