c787a6d85c6a14fe1858319789424411a7ce1b56
Aaron Tucker <aarondtucker@gmail.com>
2018-10-09 00:37:49 -0700

    tweak the scripts to work more stably

--- scripts/overfitting_tests.sh
+++ scripts/overfitting_tests.sh
@@ -9,7 +9,9 @@ while [[ "$#" > 0 ]]; do case $1 in
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
 
-case BASE_DIR in
+echo "Base directory arg: $BASE_DIR";
+
+case $BASE_DIR in
     ('enduro') ENV='EnduroNoFrameskip-v4' ;;
     ('pong') ENV='PongNoFrameskip-v4' ;;
     ('catcher') ENV='PLECatcher-v0' ;;
@@ -17,22 +19,26 @@ case BASE_DIR in
 esac;
 
 mkdir workshop/$BASE_DIR/$OUT_DIR;
+mkdir workshop/$BASE_DIR/$OUT_DIR/batched_state_only;
+mkdir workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_only;
+mkdir workshop/$BASE_DIR/$OUT_DIR/batched_state_action;
+mkdir workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_action;
 echo "Placing results in workshop/$BASE_DIR/$OUT_DIR"
 
 
 for i in 1 2 4 8 16 64; do
     echo $i;
-    base_command="python train_airl.py --env $ENV --seed $SEED --trajectories_file results/$BASE_DIR/8_trajectories_cache.pkl --n_iter 1 --discriminator_itrs 1000 --policy_update_freq 1 --ppo_itrs_in_batch $i --init_location results/$BASE_DIR/expert.pkl --ablation train_discriminator";
+    base_command="pasuspender -- python train_airl.py --env $ENV --seed $SEED --trajectories_file results/$BASE_DIR/8_trajectories_cache.pkl --n_iter 1 --discriminator_itrs 1000 --policy_update_freq 1 --ppo_itrs_in_batch $i --init_location results/$BASE_DIR/expert.pkl --ablation train_discriminator";
     out_file="buffer_size_$i.txt;";
     
     encoded="--encoder results/$BASE_DIR/encoder.pkl --reward_type mlp ";
     
     echo "state only";
-    eval "$base_command --state_only > workshop/$BASE_DIR/$OUT_DIR/batched_state_only/$out_file";
+    eval "$base_command --state_only &> workshop/$BASE_DIR/$OUT_DIR/batched_state_only/$out_file";
     echo "state only encoded";
-    eval "$base_command --state_only $encoded > workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_only/$out_file";
+    eval "$base_command --state_only $encoded &> workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_only/$out_file";
     echo "just batched";
-    eval "$base_command > workshop/$BASE_DIR/$OUT_DIR/batched_state_action/$out_file";
+    eval "$base_command &> workshop/$BASE_DIR/$OUT_DIR/batched_state_action/$out_file";
     echo "encoded";
-    eval "$base_command $encoded > workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_action/$out_file";
+    eval "$base_command $encoded &> workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_action/$out_file";
 done
--- scripts/perf_comparison.sh
+++ scripts/perf_comparison.sh
@@ -9,7 +9,7 @@ while [[ "$#" > 0 ]]; do case $1 in
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
 
-case BASE_DIR in
+case $BASE_DIR in
     ('enduro') ENV='EnduroNoFrameskip-v4' ;;
     ('pong') ENV='PongNoFrameskip-v4' ;;
     ('catcher') ENV='PLECatcher-v0' ;;
@@ -21,19 +21,21 @@ echo "Placing results in workshop/$BASE_DIR/$OUT_DIR"
 
 
 
-base_command="python train_airl.py --env $ENV --seed $SEED --trajectories_file results/$BASE_DIR/8_trajectories_cache.pkl --n_iter 100 $MODIFICATION";
+base_command="pasuspender -- python train_airl.py --env $ENV --seed $SEED --trajectories_file results/$BASE_DIR/8_trajectories_cache.pkl --n_iter 100 $MODIFICATION";
 
 batched="--ppo_itrs_in_batch 4 " 
 state_only="--state_only ";
 encoded="--encoder results/$BASE_DIR/encoder.pkl --reward_type mlp ";
 
 echo $base_command;
-eval "$base_command > workshop/$BASE_DIR/$OUT_DIR/basic.txt"&
-eval "$base_command $state_only > workshop/$BASE_DIR/$OUT_DIR/state_only.txt";
-eval "$base_command $encoded > workshop/$BASE_DIR/$OUT_DIR/encoded.txt"&
-eval "$base_command $encoded $state_only > workshop/$BASE_DIR/$OUT_DIR/encoded_state_only.txt";
+eval "$base_command &> workshop/$BASE_DIR/$OUT_DIR/basic.txt"&
+eval "$base_command $state_only &> workshop/$BASE_DIR/$OUT_DIR/state_only.txt"&
+eval "$base_command $encoded &> workshop/$BASE_DIR/$OUT_DIR/encoded.txt"&
+eval "$base_command $encoded $state_only &> workshop/$BASE_DIR/$OUT_DIR/encoded_state_only.txt"&
+wait
 
-eval "$base_command $batched > workshop/$BASE_DIR/$OUT_DIR/batched.txt"&
-eval "$base_command $batched $state_only > workshop/$BASE_DIR/$OUT_DIR/batched_state_only.txt";
-eval "$base_command $batched $encoded > workshop/$BASE_DIR/$OUT_DIR/batched_encoded.txt"&
-eval "$base_command $batched $state_only $encoded > workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_only.txt";
+eval "$base_command $batched &> workshop/$BASE_DIR/$OUT_DIR/batched.txt"&
+eval "$base_command $batched $state_only &> workshop/$BASE_DIR/$OUT_DIR/batched_state_only.txt"&
+eval "$base_command $batched $encoded &> workshop/$BASE_DIR/$OUT_DIR/batched_encoded.txt"&
+eval "$base_command $batched $state_only $encoded &> workshop/$BASE_DIR/$OUT_DIR/batched_encoded_state_only.txt"&
+wait
